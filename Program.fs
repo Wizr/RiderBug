df5c793c063bcca2f9d3408947356317ea8649fd
Xudong Yang <fanderous@gmail.com>
2023-06-30 10:27:55 +0800

    demo

---
+++ Program.fs
@@ -0,0 +1,8 @@
+﻿open System.ComponentModel.DataAnnotations
+
+type FsRangeAttribute =
+    inherit RangeAttribute
+    new(minimum: int, maximum: int) = { inherit RangeAttribute(minimum = minimum, maximum = maximum) }
+    new(minimum: double, maximum: double) = { inherit RangeAttribute(minimum = minimum, maximum = maximum) }
+
+printfn "Hello World"
